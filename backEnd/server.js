5dbe5881074849c1b865f7bac719f4b1a0e6822d
pavankumarShirol <pavankumarshirol03@gmail.com>
2024-07-11 18:49:16 +0530

    1-User-Routes-and-controllers

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js"
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as clodinary} from "cloudinary";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
 
 const app = express();
 
 dotenv.config();
+
+clodinary.config({
+    cloud_name:process.env.CLODINARY_CLOUD_NAME,
+    api_key:process.env.CLODINARY_API_KEY,
+    api_secret:process.env.CLODINARY_API_SECRET
+});
 
 const PORT = process.env.PORT;
 
@@ -19,6 +29,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is runnig on port 8000");
