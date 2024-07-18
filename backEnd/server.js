9bdfbb2c3715424334bdadf30287e7065dc37593
pavankumarShirol <pavankumarshirol03@gmail.com>
2024-07-18 17:50:44 +0530

    1-Notofication model and controller (Backend Completed)

--- backEnd/server.js
+++ backEnd/server.js
@@ -6,6 +6,7 @@ import {v2 as clodinary} from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -32,6 +33,7 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
+app.use("/api/notifications",notificationRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is runnig on port 8000");
